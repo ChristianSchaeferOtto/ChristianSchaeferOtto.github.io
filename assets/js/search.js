f0be3349abc565fdcb1cf46d360c8958e0e09aef
ChristianSchaeferOtto <christian.schaefer@otto.de>
2020-12-22 13:17:40 +0100

    fehler korrigiert

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,14 +1,14 @@
 
 (function(queryString){
   const urlParams = new URLSearchParams(queryString);
-  const product = urlParams.get('product_id');
+  const product_id = urlParams.get('product_id');
   document.write("produkt id: "+product_id);
-  const product = urlParams.get('article_id');
+  const article_id = urlParams.get('article_id');
   document.write("artikel id: "+article_id);
-  const product = urlParams.get('variant_id');
+  const variant_id = urlParams.get('variant_id');
   document.write("varianten id: "+variant_id);
-  const product = urlParams.get('sku');
+  const sku = urlParams.get('sku');
   document.write("sku: "+sku);
-  const product = urlParams.get('ean');
+  const ean = urlParams.get('ean');
   document.write("ean: "+ean);
 }(window.location.search));
