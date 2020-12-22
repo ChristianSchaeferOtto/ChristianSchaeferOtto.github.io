9de78308f53d5f71a81f7cdefb77deb8eeedfaa7
ChristianSchaeferOtto <christian.schaefer@otto.de>
2020-12-22 13:12:10 +0100

    closure

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,5 +1,5 @@
 
-function(queryString){
+(function(queryString){
   const urlParams = new URLSearchParams(queryString);
   const product = urlParams.get('product_id');
   document.write("produkt id: "+product_id);
@@ -8,7 +8,7 @@ function(queryString){
   const product = urlParams.get('variant_id');
   document.write("varianten id: "+variant_id);
   const product = urlParams.get('sku');
-  document.write("sku: "+sku);  
+  document.write("sku: "+sku);
   const product = urlParams.get('ean');
   document.write("ean: "+ean);
-}(window.location.search);
+}(window.location.search));
