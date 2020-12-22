ac9ec28c3a0206387fae9e94ecdd50882439575e
ChristianSchaeferOtto <christian.schaefer@otto.de>
2020-12-22 13:09:25 +0100

    search js

---
+++ assets/js/search.js
@@ -0,0 +1,14 @@
+
+function(queryString){
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get('product_id');
+  document.write("produkt id: "+product_id);
+  const product = urlParams.get('article_id');
+  document.write("artikel id: "+article_id);
+  const product = urlParams.get('variant_id');
+  document.write("varianten id: "+variant_id);
+  const product = urlParams.get('sku');
+  document.write("sku: "+sku);  
+  const product = urlParams.get('ean');
+  document.write("ean: "+ean);
+}(window.location.search);
