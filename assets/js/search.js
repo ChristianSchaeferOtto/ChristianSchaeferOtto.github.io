0c37e2862d9b2ca0b5890757003331f267d8cb2c
ChristianSchaeferOtto <schaefer.christian@otto.de>
2020-12-23 10:28:06 +0100

    search test 5

--- assets/js/search.js
+++ assets/js/search.js
@@ -4,8 +4,8 @@ $( document ).ready(function() {
 });
 
 function testSearch(id) {
-/*
   const base_url = "https://search.live.opal.cloud.otto.de/opal-search/products?onlineRelevance.value=in(ACTIVE,INACTIVE)&";
+/*
   $.get({
     url: base_url + "_id=" + id,
     success: testSearchSuccess,
